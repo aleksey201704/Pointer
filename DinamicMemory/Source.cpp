2ac46113952f256b81f1a9a89dee16330a2f9901
SenX_eb85@itstepru.onmicrosoft.com <SenX_eb85@itstepru.onmicrosoft.com>
2021-11-13 21:34:10 +0500

    DinamicMassiv

---
+++ DinamicMemory/Source.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int* arr, const int n);
+
+
+void main() {
+	setlocale(LC_ALL, "");
+
+	int n;
+	cout << "Вевесмти размер"; cin >> n;
+	// обьявление динамического массива
+	int* arr = new int[n];
+
+	// обраюжение элемнтов к динамического массива
+
+	FillRand(arr, n);
+	Print(arr, n);
+	
+	// удаление динамического массива.
+	delete[] arr;
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		// через арифметитку указателей и оператор разыменования
+		*(arr + i) = rand() % 100;
+	}
+}
+
+void Print(int* arr, const int n)
+
+{
+	for (int i = 0; i < n; i++)
+	{
+		// через оператор индексирования
+		cout << arr[i] << "\t";
+
+	}
+}
